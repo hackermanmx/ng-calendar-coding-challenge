d784696a89823a4314b6548e68e657a4b87da4f7
deeneon <david.uaa@gmail.com>
2019-06-10 06:11:33 +0000

    [#16] [BUG] - known errors
    - Fixes month out of range bug
    - Fixes datetime styling
    - Fixes indexedDB getter

--- ng-calendar-test/src/app/common/dialog/dialog.component.ts
+++ ng-calendar-test/src/app/common/dialog/dialog.component.ts
@@ -1,6 +1,8 @@
-import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, OnInit, Optional} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {CoreService} from '../../services/core.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {endOfMonth, format, startOfMonth} from "date-fns";
 
 @Component({
   selector: 'app-dialog',
@@ -12,12 +14,21 @@ export class DialogComponent implements OnInit {
   isQuerying = false;
   forecast = null;
   dataWrapper = null;
+  minDate = null;
+  maxDate = null;
+  canProceed = true;
   colors = ['blue', 'red', 'green', 'purple', 'gray', 'brown', 'orange'];
 
   constructor(@Optional() public dialogRef: MatDialogRef<DialogComponent>,
               public coreService: CoreService,
+              public cdr: ChangeDetectorRef,
+              public snackBar: MatSnackBar,
               @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
     this.dataWrapper = JSON.parse(JSON.stringify(this.data));
+    this.minDate = format(startOfMonth(new Date()), 'MM/dd/yyyy');
+    this.maxDate = format(endOfMonth(new Date()), 'MM/dd/yyyy');
+
+    console.log(this.minDate, this.maxDate);
   }
 
   ngOnInit() {
@@ -54,5 +65,16 @@ export class DialogComponent implements OnInit {
     form.reset();
     // this.data = this.dataWrapper;
     this.dialogRef.close();
+  }
+
+  onDateChange(evt: any) {
+    this.canProceed = new Date(evt).getMonth() === new Date().getMonth();
+    if (this.canProceed) {
+      this.data.form.date = evt;
+    } else {
+      this.snackBar.open('Free version only supports single month only!', 'See Pricing', {
+        duration: 3000,
+      });
+    }
   }
 }
